5f5a16b1846664081a0b4146b4091724c02df04a
Doc Pillman <doc.pillman@icloud.com>
2023-07-10 19:36:09 +0300

    fix cmake

--- rcmake_macros/clang.cmake
+++ rcmake_macros/clang.cmake
@@ -1,17 +1,17 @@
 # Use -DNO_STATIC_ANALYSIS=1 to suppress static analysis.
 # If not suppressed, the tools used here shall be available, otherwise the build will fail.
-if (NOT arm-none-eabi-gcc)
-if (NOT NO_STATIC_ANALYSIS)
-    # clang-tidy (separate config files per directory)
-    find_program(clang_tidy NAMES clang-tidy)
-    if (NOT clang_tidy)
-        message(FATAL_ERROR "Could not locate clang-tidy")
+if (arm-none-eabi-gcc)
+    if (NOT NO_STATIC_ANALYSIS)
+        # clang-tidy (separate config files per directory)
+        find_program(clang_tidy NAMES clang-tidy)
+        if (NOT clang_tidy)
+            message(FATAL_ERROR "Could not locate clang-tidy")
+        endif ()
+        message(STATUS "Using clang-tidy: ${clang_tidy}")
+        set(CMAKE_C_CLANG_TIDY ${clang_tidy})
+        set(CMAKE_CXX_CLANG_TIDY ${clang_tidy})
     endif ()
-    message(STATUS "Using clang-tidy: ${clang_tidy}")
-    set(CMAKE_C_CLANG_TIDY ${clang_tidy})
-    set(CMAKE_CXX_CLANG_TIDY ${clang_tidy})
 endif ()
-endif()
 
 # clang-format
 find_program(clang_format NAMES clang-format)
